05ea7ee2021fa134048edd8faa44e23d5789cd45
Vitaliia <talina.krina@gmail.com>
2025-11-25 00:08:27 +0300

    lab3

--- lab3/news/delete_news.inc.php
+++ lab3/news/delete_news.inc.php
@@ -1,1 +1,17 @@
 <?php
+if (!isset($_GET['del'])) {
+    return;
+}
+
+$id = intval($_GET['del']);
+if ($id <= 0) {
+    header("Location: news.php");
+    exit;
+}
+
+if (!$news->deleteNews($id)) {
+    $errMsg = "Произошла ошибка при удалении новости";
+} else {
+    header("Location: news.php");
+    exit;
+}
