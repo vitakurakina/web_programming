2eb13c19c8f4c7942b270aa6139e46625bc7803b
vitakurakina <talina.krina@gmail.com>
2025-12-02 14:28:44 +0300

    lab1-7

--- lab8_1/project/config/connection.php
+++
@@ -1,6 +0,0 @@
-<?php
-	define('DB_HOST', 'localhost');
-	define('DB_USER', 'n93077gb_mvc');
-	define('DB_PASS', 'Qwerty123_');
-	define('DB_NAME', 'n93077gb_mvc');
-
--- lab8_1/project/views/hello/index.php
+++
@@ -1,3 +0,0 @@
-<h1>Все работает!</h1>
-<p>Проверка отображения картинки:</p>
-<img src="<?= BASE_PATH ?>/project/webroot/logo.jpg" alt="Логотип MVC Framework">
